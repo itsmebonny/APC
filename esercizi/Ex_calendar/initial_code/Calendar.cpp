ff5c898557b7094e437cbc2a79cd905283324065
bonni <andrea.bonifacio.000@gmail.com>
2022-11-29 19:23:43 +0100

    nknk

--- esercizi/Ex_calendar/initial_code/Calendar.cpp
+++ esercizi/Ex_calendar/initial_code/Calendar.cpp
@@ -6,9 +6,22 @@ Calendar::Calendar(Calendar &c):events(c.events){}
 void Calendar::addEvent(const Event& e){
     events.push_back(e);
 }
-void Calendar::updateEvent (time_t, const std::string&, time_t, const std::string&){
-
+void Calendar::updateEvent (time_t t0, const std::string& n0, time_t t1, const std::string& n1){
+    for (size_t i = 0; i < events.size(); i++)
+    {
+        if (events[i].getName() == n0 && events[i].getTime() == t0)
+        {
+            events[i].setName(n1);
+            events[i].setTime(t1);
+        }
+        
+    }
 }
 void Calendar::print()const{
-
+    for (size_t i = 0; i < events.size(); i++)
+    {
+        std::cout << events[i].getTime() << " " << events[i].getName() << " ";
+    }
+    std::cout << std::endl;
+    
 }
