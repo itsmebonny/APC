719f82b3895da92bce6997968e0a447a68912bda
bonni <andrea.bonifacio.000@gmail.com>
2022-12-13 16:33:07 +0100

    bhbh

--- lab_4/search.cc
+++ lab_4/search.cc
@@ -37,6 +37,38 @@ bool binary_search(const std::vector<Arcade::Player> &player_vec, unsigned playe
 		return false;
 	}
 }
+bool stl_binary_search(const std::vector<Arcade::Player> &player_vec, unsigned player_username){
+	timing::time_point start = std::chrono::steady_clock::now();
+
+	std::vector<Arcade::Player>::const_iterator begin = player_vec.begin(), end = player_vec.end(),
+	// original musernamepoint
+	musername = begin + (end - begin) / 2;
+	unsigned n_iter = 1;
+
+	while (begin != end and musername->getUsername() != player_username) {
+		/* end is meant to be invalusername, so in both cases
+		* we are ignoring musername at the following iteration
+		*/
+		if (player_username < musername->getUsername())
+		end = musername;
+		else
+		begin = musername + 1;
+
+		musername = begin + (end - begin) / 2;
+		++n_iter;
+	}
+
+	std::cout << "Number of Iterations " << n_iter << "" << std::endl;
+
+	timing::time_point finish = std::chrono::steady_clock::now();
+	timing::elapsed_between(start, finish);
+
+	if (musername != player_vec.end() and player_username == musername->getUsername()) {
+		return true;
+	} else {
+		return false;
+	}
+}
 bool set_search(const std::set<Arcade::Player> &player_set, unsigned player_username){
 	timing::time_point start = std::chrono::steady_clock::now();
 	bool check = false;
@@ -47,10 +79,15 @@ bool set_search(const std::set<Arcade::Player> &player_set, unsigned player_user
 		{
 			check = true;
 			it_username = player_set.end();
-		}
-		
+		}	
 	}
-	
+	timing::time_point finish = std::chrono::steady_clock::now();
+	timing::elapsed_between(start, finish);
+	return check;
+}
+bool map_search(const std::unordered_map<unsigned, Arcade::Player> &player_map, unsigned player_username){
+	timing::time_point start = std::chrono::steady_clock::now();
+	bool check = player_map.find(player_username) != player_map.end();
 	timing::time_point finish = std::chrono::steady_clock::now();
 	timing::elapsed_between(start, finish);
 	return check;
